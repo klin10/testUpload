5be0bd7c24a5a9a60a9822b1d69dd0d4e8f175a8
jlin53 <jlin53@ucsc.edu>
2017-07-14 06:20:08 +0000

    revmoe proc

--- upload-manager.php
+++ upload-manager.php
@@ -1,7 +1,6 @@
 <?php
 // Check if the form was submitted
 
-
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Check if file was uploaded without errors
     if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
@@ -17,7 +16,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Verify MYME type of the file
         if(in_array($filetype, $allowed)){
             // Check whether file exists before uploading it
-            if(file_exists("public/" . $_FILES["photo"]["name"])){
+            if(file_exists("upload/" . $_FILES["photo"]["name"])){
                 echo $_FILES["photo"]["name"] . " is already exists.";
             } else{
                 move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $_FILES["photo"]["name"]);
