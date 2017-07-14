8eaf3de7ed5e517c8357057da3c35c93e3ae6d7b
jlin53 <jlin53@ucsc.edu>
2017-07-14 04:45:47 +0000

    deploy

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+  echo "done";
+
+} else{
+  echo "faild";
+}
